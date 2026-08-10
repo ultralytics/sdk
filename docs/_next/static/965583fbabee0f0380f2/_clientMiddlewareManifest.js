d2f06b9d3b82a27b151f130b96a8d3532f4e67bb
UltralyticsAssistant <web@ultralytics.com>
2026-08-10 20:14:02 +0000

    Auto-format by https://ultralytics.com/actions

--- docs/_next/static/965583fbabee0f0380f2/_clientMiddlewareManifest.js
+++ docs/_next/static/965583fbabee0f0380f2/_clientMiddlewareManifest.js
@@ -1,1 +1,3 @@
+// Ultralytics 🚀 AGPL-3.0 License - https://ultralytics.com/license
+
 self.__MIDDLEWARE_MATCHERS = [];self.__MIDDLEWARE_MATCHERS_CB && self.__MIDDLEWARE_MATCHERS_CB()
--- docs/_next/static/965583fbabee0f0380f2/_ssgManifest.js
+++ docs/_next/static/965583fbabee0f0380f2/_ssgManifest.js
@@ -1,1 +1,3 @@
+// Ultralytics 🚀 AGPL-3.0 License - https://ultralytics.com/license
+
 self.__SSG_MANIFEST=new Set([]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
